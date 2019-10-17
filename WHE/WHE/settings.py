1befa1ff170f9c6aacb967bdd1e29a1e0f788d25
arunikaydav42 <arunikayadav42@gmail.com>
2019-10-17 11:36:27 +0530

    css and js files separated from html files

--- WHE/WHE/settings.py
+++ WHE/WHE/settings.py
@@ -122,6 +122,6 @@ STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = 'media'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS=(
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
